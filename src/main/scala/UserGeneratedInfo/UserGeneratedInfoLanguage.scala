4459fa6d942980e70e0cab0bc8f118f24430172f
romue404 <robert.mueller1990@googlemail.com>
2017-02-14 13:52:05 +0100

    add user generated info lanuage

--- src/main/scala/UserGeneratedInfo/UserGeneratedInfoLanguage.scala
+++ src/main/scala/UserGeneratedInfo/UserGeneratedInfoLanguage.scala
@@ -9,7 +9,28 @@ class UserGeneratedInfoLanguage extends RegexParsers {
 
   /*=====ACTION MAPPINGS=====*/
   import UserGeneratedInfoActor.{GetAllProposals, AllProposals,
-    AddProposal, AddedProposal,
+    AddProposal,
     Upvote, Downvote, DeleteProposal
   }
+
+  def addProposal = ("create info"|"new info"|"make info")~>("name"|"key")~>opt("=")~>word~("info"|"value")~opt("=")~anyString~opt("message"|"msg" ~> opt("=") ~> anyString) ^^ {
+    case (infoName~_~_~value~message) => AddProposal(ValueProposal(infoName, value, message))
+  }
+
+  def upvote = ("+1"|"+"|"upvote"|"up")~>word~withUser ^^ {
+    case(infoName~userName)=> Upvote(infoName, userName)
+  }
+
+  def downvote = ("-1"|"-"|"downvote"|"down")~>word~withUser ^^ {
+    case(infoName~userName)=> Downvote(infoName, userName)
+  }
+
+  def delete = ("delete"|"remove"|"kill")~>word ^^ {
+    case(infoName)=> DeleteProposal(infoName)
+  }
+
+  def word: Parser[String]    = """[a-zA-Z1-9_:-]+""".r ^^ { _.toString }
+  def anyString: Parser[String] = """.*""".r ^^ {_.toString}
+  def user = ("me"|"username"|"user")
+  def withUser: Parser[String] = user ~> opt("=") ~> word
 }
